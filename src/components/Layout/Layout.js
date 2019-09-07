fbddddf0b33b33650d0558ae14afa441a2d03f90
david dobson <david.m.dobson@hotmail.com>
2019-09-07 13:02:09 -0400

    Added Sidebar, logo, navbar,  and made it dynamic and added css

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import Aux from '../../hoc/Auxiliary';
-import styles from './Layout.module.css'
+import Auxiliary from '../../hoc/Auxiliary';
+import styles from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
-    <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+    <Auxiliary>
+        <Toolbar />
+        <SideDrawer />
         <main className={styles.Content}>
             {props.children}
         </main>
-    </Aux>
+    </Auxiliary>
 );
 
 export default layout;
